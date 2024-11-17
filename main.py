214f9fc2337049b8cb1424e35c299e2cc432f739
Youta <0x59.xxx@gmail.com>
2024-11-17 23:38:49 +0900

    Confirm

--- main.py
+++ main.py
@@ -24,7 +24,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "https://fancloud.vercel.app"
+        "https://fancloud.vercel.app",
+        ""
     ],
     allow_credentials=True,
     allow_methods=["*"], 
